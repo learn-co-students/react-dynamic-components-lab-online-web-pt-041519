c46dd813f088ea3be7d398b89641d68d7bc78bbf
Alex Merecka <merecka@gmail.com>
2020-01-10 12:49:09 -0600

    Done.

--- README.md
+++ README.md
@@ -120,3 +120,28 @@ export default class Example extends Component {
       <div className="color-box" style={{opacity: this.props.opacity}}>   //Note: The style attribute accepts a JavaScript object with camelCased properties rather than a CSS string
         { this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity - 0.1} /> : null }
       </div>
+
+
+
+
+
+
+      import React, { Component } from 'react';
+
+export default class ColorBox extends Component {
+
+  render() {
+
+    if (this.props.opacity >= 0.2) {
+
+      return (
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+         <ColorBox opacity={this.props.opacity - 0.1} />
+        </div>
+       )
+    } else {
+      return null
+    }
+
+  }
+}
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,17 +3,17 @@ import React, { Component } from 'react';
 export default class ColorBox extends Component {
 
   render() {
-  	debugger
-  	if (this.props.opacity >= 0.2) {
 
-  		return (
-	      <div className="color-box" style={{opacity: (this.props.opacity)}}>   //Note: The style attribute accepts a JavaScript object with camelCased properties rather than a CSS string
-	       <ColorBox opacity={this.props.opacity - 0.1} />
-	      </div>
-   		 )
-  	} else {
-  		return null
-  	}
+    if (this.props.opacity >= 0.2) {
+
+      return (
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+         <ColorBox opacity={this.props.opacity - 0.1} />
+        </div>
+       )
+    } else {
+      return null
+    }
 
   }
 }
