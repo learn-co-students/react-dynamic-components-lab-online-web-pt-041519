463d9797f3d3c36b4e5c5b7d001120bc18c190a2
Sarah <saranderson13@gmail.com>
2020-01-20 17:01:57 -0500

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,11 +4,22 @@ export default class ColorBox extends Component {
 
   render() {
     return (
-      <div className="color-box" style={{opacity: 2}}>   //Note: The style attribute accepts a JavaScript object with camelCased properties rather than a CSS string
+      <div className="color-box" opacity={this.props.opacity}>   
         {/* your conditional code here! */}
+        
       </div>
     )
   }
+
+  // reRender(propOpacity) {
+  //   let nonFloat = propOpacity * 100
+    
+  //   while( nonFloat > 20) {
+  //     console.log(nonFloat)
+  //     <ColorBox opacity={nonFloat / 100} />
+  //     nonFloat -= 10
+  //   }
+  // }
 
 }
 
